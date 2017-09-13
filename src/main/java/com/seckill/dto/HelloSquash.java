338895c38aa3985076388691e5af19b5a7c7d03a
houwanxing1 <houwanxing1@jd.com>
2017-09-13 11:02:18 +0800

    新增HelloSquash文件。
    HelloSquash文件新增Javadoc。

---
+++ src/main/java/com/seckill/dto/HelloSquash.java
@@ -0,0 +1,10 @@
+package com.seckill.dto;
+
+/**
+ * @author houwanxing1  2017-09-13
+ */
+public class HelloSquash {
+    public static void main(String[] args) {
+        System.out.println("Hello,Squash.");
+    }
+}
